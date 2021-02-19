97f065877706cb3250a928729f32843c95856002
Nicole <43906055+Knotonium@users.noreply.github.com>
2021-02-19 10:40:05 +0100

    Update Largest_Prime_Factor.java

--- Largest_Prime_Factor.java
+++ Largest_Prime_Factor.java
@@ -1,3 +1,7 @@
+// Problem 3:
+// The prime factors of 13195 are 5, 7, 13 and 29.
+// What is the largest prime factor of the number 600851475143 ?
+
 public class Largest_Prime_Factor {
 	public static void main(String[] args) {
 		long a = 600851475143L;
