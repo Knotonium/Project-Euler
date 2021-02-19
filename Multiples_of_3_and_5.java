361346b05c32735c2de3d5d35831b871cf4da791
Nicole <43906055+Knotonium@users.noreply.github.com>
2021-02-19 10:37:26 +0100

    Update Multiples_of_3_and_5.java

--- Multiples_of_3_and_5.java
+++ Multiples_of_3_and_5.java
@@ -1,3 +1,7 @@
+// Problem 1:
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
 public class Multiples_of_3_and_5 {
 	public static void main (String[] args) {
 		int MAX = 1000, multi_3 = 3, multi_5 = 5, sum = 0;
